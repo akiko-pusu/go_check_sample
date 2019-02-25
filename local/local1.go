556863380055d51469a195fd77f55782dc12fad1
Akiko Takano <akiko.pusu@gmail.com>
2019-02-25 20:40:54 +0900

    Omit string statement.

--- local/local1.go
+++ local/local1.go
@@ -1,6 +1,6 @@
 package local
 
 // 小文字で始まるとパッケージ内のみ：同一パッケージからしか呼べません.
-var localStr string = "Localだよ"    // <- Package local 内でのみ参照可能
+var localStr = "Localだよ" // <- Package local 内でのみ参照可能
 // 大文字で始まるとパッケージ外参照可能.
-var GlobalStr string = "Package localのGlobalStrという変数だよ" // <- Package 外に公開
+var GlobalStr = "Package localのGlobalStrという変数だよ" // <- Package 外に公開
